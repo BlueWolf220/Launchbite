332aadce81b4791f65fc1893d4f2e1e2e153dc64
BlueWolf220 <60710307+BlueWolf220@users.noreply.github.com>
2020-03-30 18:18:05 -0500

    remake commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<img src='Launchbite_logo.png'>
+# Launchbite
+ A simple app to open apps, like Fusion360 and Mu, and is easily customizable. It is written in pure python.
+
+ All you need to learn to customize this app is minor Tkinter, and the keyboard module.
+I decided to use the logos of the apps, but only to make customization easier. This desktop is only compatible with windows, and is filled with apps I open most often.
+
+The app requires a little bit of setup, but it is just putting apps on the toolbar. from left to right the apps go: files, mail, Fusion 360, Eagle, chrome, GitKraken, GitHub desktop, atom, and mu.
---
+++ custom desktop.py
@@ -0,0 +1,43 @@
+#imports
+import keyboard
+from tkinter import *
+import time
+
+#general setup
+window = Tk()
+window.title('Launchdesk')
+
+#image setup
+
+#function setup
+def open_chrome():
+    keyboard.press_and_release('windows+5')
+def open_gitkraken():
+    keyboard.press_and_release('windows+6')
+def open_fusion():
+    keyboard.press_and_release('windows+3')
+def open_eagle():
+    keyboard.press_and_release('windows+4')
+def open_githubDesk():
+    keyboard.press_and_release('windows+7')
+def open_atom():
+    keyboard.press_and_release('windows+8')
+def open_mu():
+    keyboard.press_and_release('windows+9')
+#button setup
+chromebutton = Button(window, text = 'chrome', command = open_chrome)
+gitkrakenButton = Button(window, text = 'GitKraken', command = open_gitkraken)
+fusionButton = Button(window, text='Fusion 360', command = open_fusion)
+eagleButton = Button(window, text='Eagle', command = open_eagle)
+githubDeskButton = Button(window, text='Github Desktop', command = open_githubDesk)
+atomButton = Button(window, text='Atom', command = open_atom)
+muButton = Button(window, text='Mu', command = open_mu)
+
+#load and launch
+chromebutton.pack()
+gitkrakenButton.pack()
+eagleButton.pack()
+githubDeskButton.pack()
+atomButton.pack()
+muButton.pack()
+window.mainloop()
